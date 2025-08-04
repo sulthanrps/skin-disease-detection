f29ab9bb063593ec013bad47901ea85e44461228
Sulthan Rafi <57024975+sulthanrps@users.noreply.github.com>
2025-08-04 16:17:06 +0000

    add mobile responsivitiy to dashboard sidebar

--- skin-disease-detector/src/app/components/DashboardPage/Sidebar.tsx
+++ skin-disease-detector/src/app/components/DashboardPage/Sidebar.tsx
@@ -10,24 +10,24 @@ interface ISideBarProps {
 const Sidebar: React.FC<ISideBarProps> = ({activeBar, onTabChange}) => {
     return (
         <div className="flex flex-col w-[20%] bg-gradient-to-br from-pink-400 to-rose-500 min-h-screen p-4">
-            <div className="flex justify-center items-center -ml-8">
-                <img src="/logo.png" alt="logo" className="w-24"/>
-                <h1 className="text-4xl font-bold -ml-4">SkinSnap</h1>
+            <div className="flex justify-center items-center md:-ml-8">
+                <img src="/logo.png" alt="logo" className="w-20 md:w-24"/>
+                <h1 className="text-4xl font-bold -ml-4 hidden md:block">SkinSnap</h1>
             </div>
             <div className="p-4 flex flex-col gap-8 mt-8">
-                <div className={`flex gap-3 items-center font-semibold text-xl p-4 pl-6 rounded-lg backdrop-blur-sm ${activeBar === "Dashboard" ? "bg-white/40 text-white border-2 border-grey" : "bg-white/20"}`} onClick={() => onTabChange('Dashboard')}>
+                <div className={`flex gap-3 items-center font-semibold text-xl p-2 hover:cursor-pointer hover:text-white transition duration-200 easeInOut md:p-4 md:pl-6 rounded-lg backdrop-blur-sm ${activeBar === "Dashboard" ? "bg-white/40 text-white border-2 border-grey" : "bg-white/20"}`} onClick={() => onTabChange('Dashboard')}>
                     <Home />
-                    <Link href="">Dashboard</Link>
+                    <Link href="" className="hidden md:block">Dashboard</Link>
                 </div>
 
-                <div className={`flex gap-3 items-center font-semibold text-xl p-4 pl-6 rounded-lg backdrop-blur-sm ${activeBar === "Snap" ? "bg-white/40 text-white border-2 border-grey" : "bg-white/20"}`} onClick={() => onTabChange('Snap')}>
+                <div className={`flex gap-3 items-center font-semibold text-xl p-2 hover:cursor-pointer hover:text-white transition duration-200 easeInOut md:p-4 md:pl-6 rounded-lg backdrop-blur-sm ${activeBar === "Snap" ? "bg-white/40 text-white border-2 border-grey" : "bg-white/20"}`} onClick={() => onTabChange('Snap')}>
                     <Focus />
-                    <Link href="">Snap</Link>
+                    <Link href="" className="hidden md:block">Snap</Link>
                 </div>
 
-                <div className={`flex gap-3 items-center font-semibold text-xl p-4 pl-6 rounded-lg backdrop-blur-sm ${activeBar === "Map" ? "bg-white/40 text-white border-2 border-grey" : "bg-white/20"}`} onClick={() => onTabChange('Map')}>
+                <div className={`flex gap-3 items-center font-semibold text-xl p-2 hover:cursor-pointer hover:text-white transition duration-200 easeInOut md:p-4 md:pl-6 rounded-lg backdrop-blur-sm ${activeBar === "Map" ? "bg-white/40 text-white border-2 border-grey" : "bg-white/20"}`} onClick={() => onTabChange('Map')}>
                     <Map />
-                    <Link href="">Map</Link>
+                    <Link href="" className="hidden md:block">Map</Link>
                 </div>
             </div>
             
--- skin-disease-detector/src/app/map/page.tsx
+++ skin-disease-detector/src/app/map/page.tsx
@@ -2,7 +2,7 @@ const NearbyDoctor = () => {
     return (
         <div className="w-[80%] flex flex-col justify-center items-center gap-4">
             <img src="/coming-soon.svg" alt="coming soon icon" className="w-[70%]" />
-            <h1 className="font-bold text-3xl">Hang on..., Map is under construction</h1>
+            <h1 className="font-bold text-3xl text-center">Hang on..., Map is under construction</h1>
         </div>
     )
 }
